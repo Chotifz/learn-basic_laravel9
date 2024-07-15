d6fe3c0168ef9da74089dd4baa0813374d224cb8
Chotifz <chotife@gmail.com>
2024-07-15 11:54:31 +0700

    tes

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    function index (){
+        echo "Hello World";
+    }
+
+    function tes (){
+        echo "Hello Tes";
+    }
+
+}
